86496e15473961308f4abda9ec8fa9fd4fdd4de5
Anna <gryshchenko.anna.rom@gmail.com>
2019-07-30 20:24:21 +0300

    delete

--- react/client/src/components/backdrop.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import "./backdrop.css"
-
-const Backdrop = ({ children }) => (
-    <div className="Backdrop">{ children }</div> 
-);
-
-export default Backdrop;
--- react/client/src/services/storege.js
+++
@@ -1,9 +0,0 @@
-export const set = value => {
-  localStorage.setItem('notes-app', JSON.stringify(value));
-};
-
-export const get = () => {
-  const data = localStorage.getItem('notes-app');
-
-  return data ? JSON.parse(data) : null;
-};
